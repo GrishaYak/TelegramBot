77cade6bf30fd5044c05ca9f3a49a15f74a52e83
GrishaYak <gishayak2@gmail.com>
2025-05-01 14:17:12 +0500

    Безобидный бот

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Импортируем необходимые классы.
 import logging
-from telegram.ext import Application, MessageHandler, filters, CommandHandler
+from telegram.ext import Application, MessageHandler, filters, CommandHandler, ConversationHandler
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
 import os
 from dotenv import load_dotenv
@@ -10,71 +10,63 @@ BOT_TOKEN = os.getenv('BOT_TOKEN')
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-reply_keyboard = [['/set', '/help'],
-                  ['/unset', '/close']]
-markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+reply_keyboard = [['/start', '/help']]
+markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
 
 
 async def start(update, context):
-    user = update.effective_user
-    await update.message.reply_html(
-        rf"Привет {user.mention_html()}! Я бот Гриши. Я почти полностью скопирован с учебника Яндекс Лицея!",
-        reply_markup=markup)
+    await update.message.reply_text(
+        "Привет. Пройдите небольшой опрос, пожалуйста!\n"
+        "Вы можете прервать опрос, послав команду /stop.\n"
+        "В каком городе вы живёте?")
+    return 1
+
+
+async def first_response(update, context):
+    locality = update.message.text
+    await update.message.reply_text(
+        f"Какая погода в городе {locality}?")
+    return 2
+
+
+async def second_response(update, context):
+    weather = update.message.text
+    logger.info(weather)
+    await update.message.reply_text("Какие цифры написаны на обороте вашей банковской карты?")
+    return 3
+
+
+async def bye(update, context):
+    logger.info(update.message.text)
+    await update.message.reply_text("Всего доброго!")
+
+
+async def stop(update, context):
+    logger.info(update.message.text)
+    await update.message.reply_text("Всего доброго!")
+    return ConversationHandler.END
 
 
 async def help_command(update, context):
+    keyboard = [['/start']]
+    markup1 = ReplyKeyboardMarkup(keyboard, one_time_keyboard=True)
     await update.message.reply_text(
-        "Я бот с задачами по таймеру.")
-
-
-TIMER = 5  # таймер на 5 секунд
-
-
-def remove_job_if_exists(name, context):
-    """Удаляем задачу по имени.
-    Возвращаем True если задача была успешно удалена."""
-    current_jobs = context.job_queue.get_jobs_by_name(name)
-    if not current_jobs:
-        return False
-    for job in current_jobs:
-        job.schedule_removal()
-    return True
-
-
-async def task(context):
-    """Выводит сообщение"""
-    await context.bot.send_message(context.job.chat_id, text=f'КУКУ! 5c. прошли!')
-
-
-async def set_timer(update, context):
-    """Добавляем задачу в очередь"""
-    chat_id = update.effective_message.chat_id
-    job_removed = remove_job_if_exists(str(chat_id), context)
-    context.job_queue.run_once(task, TIMER, chat_id=chat_id, name=str(chat_id), data=TIMER)
-
-    text = f'Вернусь через 5 с.!'
-    if job_removed:
-        text += ' Старая задача удалена.'
-    await update.effective_message.reply_text(text)
-
-
-async def unset(update, context):
-    """Удаляет задачу, если пользователь передумал"""
-    chat_id = update.message.chat_id
-    job_removed = remove_job_if_exists(str(chat_id), context)
-    text = 'Таймер отменен!' if job_removed else 'У вас нет активных таймеров'
-    await update.message.reply_text(text)
+        "Я абсолютно безобидный бот. Хочешь со мной поболтать? Тогда напиши /start", reply_markup=markup1)
 
 
 def main():
     application = Application.builder().token(BOT_TOKEN).build()
-
-    application.add_handler(CommandHandler('set', set_timer))
-    application.add_handler(CommandHandler("unset", unset))
-    application.add_handler(CommandHandler("help", help_command))
-    application.add_handler(CommandHandler("start", start))
-    application.run_polling()
-
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+        states={
+            1: [MessageHandler(filters.TEXT & ~filters.COMMAND, first_response)],
+            2: [MessageHandler(filters.TEXT & ~filters.COMMAND, second_response)],
+            3: [MessageHandler(filters.TEXT & ~filters.COMMAND, bye)]
+        },
+        fallbacks=[CommandHandler('stop', stop)]
+    )
+    application.add_handler(CommandHandler('help', help_command))
+    application.add_handler(conv_handler)
     application.run_polling()
 
 
