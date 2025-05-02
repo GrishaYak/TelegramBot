e599d9fa9f248068d16c1915dd7ea754d7e994fd
GrishaYak <gishayak2@gmail.com>
2025-05-03 00:01:04 +0500

    создал бд

---
+++ db/init.sql
@@ -0,0 +1,22 @@
+create table Users(
+    tg_username varchar[32] primary key
+)
+
+create table Categories(
+    id serial unique,
+    name varchar[32],
+    user_id varchar[32],
+    primary key (name, user_id),
+    foreign key (user_id) references Users(tg_username) on delete cascade
+)
+
+create table Alterations(
+    id serial primary key,
+    user_id varchar[32],
+    category_id int,
+    summa int,
+    description varchar[255],
+    date date,
+    foreign key (user_id) references Users(tg_username) on delete cascade,
+    foreign key (category_id) references Categories(id) on delete cascade
+)
