e7aae5e52e51fb72961b1a19423995b09ec555e9
GrishaYak <gishayak2@gmail.com>
2025-05-03 13:46:27 +0500

    Работаем

--- Markups.py
+++ Markups.py
@@ -4,5 +4,5 @@ from telegram import ReplyKeyboardMarkup
 def get_markup(preset=1):
     match preset:
         case 1:
-            keyboard = [['/set', '/help', '/unset']]
+            keyboard = [['/consumption', '/income'], ['/checkout', '/delete_me']]
             return ReplyKeyboardMarkup(keyboard)
--- main.py
+++ main.py
@@ -4,72 +4,76 @@ from telegram import ReplyKeyboardRemove
 import os
 from dotenv import load_dotenv
 from Markups import get_markup
-import sqlite3
+import asyncio
+import psycopg2
+from constants import *
+
+if os.name == 'nt':
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
 load_dotenv()
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
+CONNECTION = psycopg2.connect(database="FinancesBot", user="postgres", password="123")
+CURSOR = CONNECTION.cursor()
+
 
 async def start(update, context):
     user = update.effective_user
-    await update.message.reply_html(
-        rf"Привет {user.mention_html()}! Я бот Гриши. Я почти полностью скопирован с учебника Яндекс Лицея!",
-        reply_markup=get_markup())
+    CURSOR.execute(f"SELECT * FROM users where tg_username='{user.username}'")
+    if not CURSOR.fetchall():
+        await update.message.reply_html(
+            rf'''Привет {user.mention_html()}!
+Я бот для записи изменений на вашем финансовом счету. Приятно познакомиться!''', reply_markup=get_markup())
+        CURSOR.execute(f"INSERT INTO users values ('{user.username}');")
+        CONNECTION.commit()
+        print("added", user.username)
+    await update.message.reply_text("Вы можете добавить расход, написав */consumption*, добавить доход, "
+                                    "написав */income*, или "
+                                    "посмотреть, и, возможно, редактировать изменения на счету, написав */check*."
+                                    "Также вы можете удалить все данные о себе, если напишите */delete_me*",
+                                    reply_markup=get_markup(1))
+    return 1
 
 
 async def help_command(update, context):
-    await update.message.reply_text("Я бот с задачами по таймеру.")
+    await update.message.reply_text(HELP_TXT)
 
 
-TIMER = 5  # таймер на 5 секунд
+async def add_consumption(update, context):
+    pass
 
 
-def remove_job_if_exists(name, context):
-    """Удаляем задачу по имени.
-    Возвращаем True если задача была успешно удалена."""
-    current_jobs = context.job_queue.get_jobs_by_name(name)
-    if not current_jobs:
-        return False
-    for job in current_jobs:
-        job.schedule_removal()
-    return True
+async def add_income(update, context):
+    pass
 
 
-async def task(context):
-    """Выводит сообщение"""
-    await context.bot.send_message(context.job.chat_id, text=f'КУКУ! 5c. прошли!')
+async def checkout(update, context):
+    pass
 
 
-async def set_timer(update, context):
-    """Добавляем задачу в очередь"""
-    chat_id = update.effective_message.chat_id
-    job_removed = remove_job_if_exists(str(chat_id), context)
-    context.job_queue.run_once(task, TIMER, chat_id=chat_id, name=str(chat_id), data=TIMER)
-
-    text = f'Вернусь через 5 с.!'
-    if job_removed:
-        text += ' Старая задача удалена.'
-    await update.effective_message.reply_text(text)
-
-
-async def unset(update, context):
-    """Удаляет задачу, если пользователь передумал"""
-    chat_id = update.message.chat_id
-    job_removed = remove_job_if_exists(str(chat_id), context)
-    text = 'Таймер отменен!' if job_removed else 'У вас нет активных таймеров'
-    await update.message.reply_text(text)
+async def delete_account(update, context):
+    pass
 
 
 def main():
     application = Application.builder().token(os.getenv('BOT_TOKEN')).build()
-
-    application.add_handler(CommandHandler('set', set_timer))
-    application.add_handler(CommandHandler("unset", unset))
+    start_handler = CommandHandler("start", start)
+    consumption_handler = CommandHandler("consumption", add_consumption)
+    income_handler = CommandHandler("income", add_income)
+    checkout_handler = CommandHandler("checkout", checkout)
+    delete_account_handler = CommandHandler("delete_me", delete_account)
+    start_conv = ConversationHandler(
+        entry_points=[start_handler],
+        states={
+            1: [consumption_handler, income_handler, checkout_handler, delete_account_handler]
+        },
+        fallbacks=[start_handler]
+    )
     application.add_handler(CommandHandler("help", help_command))
-    application.add_handler(CommandHandler("start", start))
-    application.run_polling()
-
+    application.add_handler(start_conv)
     application.run_polling()
 
 
